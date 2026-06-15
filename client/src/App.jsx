63098c930d13be5cf3e48d8fbb404775d955d726
SwamirajMagar <swamirajmagar8@gmail.com>
2026-06-15 17:31:23 +0530

    room details aminities

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import AllRoom from "./pages/AllRoom";
+import RoomDetails from "./pages/RoomDetails";
 
 const App = ()=>{
     const isowner = useLocation().pathname.includes("owner")
@@ -15,6 +16,8 @@ const App = ()=>{
             <Routes>
                 <Route path='/' element={<Home/>} />
                 <Route path='/rooms' element={<AllRoom/>} />
+                <Route path='/rooms/:id' element={<RoomDetails/>} />
+
             </Routes>
            </div>
            <Footer />
